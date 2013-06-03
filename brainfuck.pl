4c117cca98d7e2536b3aa245c63cbed506bc9410
Anthony Reid <anthonyreid99@gmail.com>
2013-06-03 17:36:12 -0400

    Now using a hash with simulated function pointers to drive program

--- brainfuck.pl
+++ brainfuck.pl
@@ -1,7 +1,19 @@
-use strict;
+#use strict;
 use warnings;
+
 #Command line arg parser
 use Getopt::Std;
+
+#function declarations
+sub inc_ptr;
+sub dec_ptr;
+sub inc_val;
+sub dev_val;
+sub output_val;
+sub input_val;
+sub cond_z;
+sub cond_nz;
+sub is_whitespace;
 
 #Array for command line args
 my %opts=();
@@ -12,12 +24,36 @@ getopts("f:", \%opts);
 #If a file name was given
 if ($opts{f}) {
   #Open, or on error, die and store error in $!
-  open file, opts{f} or die $!
+  open INFILE, $opts{f} or die $!;
 }
 #TODO: else, read from stdin
 
+#Create a hash mapping symbols to functions
+my %symbol_table = (
+  ">" => "inc_ptr",
+  "<" => "dec_ptr",
+  "+" => "inc_val",
+  "-" => "dec_val",
+  "." => "output_val",
+  "," => "input_val",
+  "[" => "cond_z",
+  "]" => "cond_nz"
+);
 
+#Holds symbols read in
+my $symbol;
 
-
-
+#Read byte by byte until EOF
+while (read INFILE, $symbol, 1) {
+  #Call the function corresponding to the symbol
+  if (exists($symbol_table{$symbol})) {
+    #Treat the value string in the map as a function
+    #name and call it
+    &{$symbol_table{$symbol}}();
+  }
+  #Throw error and quit on invalid symbol
+  elsif (!is_whitespace($symbol)) {
+    die $!;
+  }
+}
 
